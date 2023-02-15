af613dca93cb1f869dd76c997939838355f6ae3f
Andrey Shpak <anshpak.by@gmail.com>
2023-02-16 01:13:19 +0300

    feat: add class Numbers

---
+++ first-lab/src/Numbers/Numbers.java
@@ -0,0 +1,11 @@
+package Numbers;
+
+public class Numbers {
+    private String numericalSequence;
+    public String getNumericalSequence(){
+        return numericalSequence;
+    }
+    public void setNumericalSequence(String numericalSequence){
+        this.numericalSequence = numericalSequence;
+    }
+}
--- first-lab/src/main/Main.java
+++ first-lab/src/main/Main.java
@@ -2,6 +2,14 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "1 2 3";
+        String res = "";
+        String[] numbers = str.split(" ");
+        for (String num : numbers) {
+            if(Integer.parseInt(num) % 3 == 0 || Integer.parseInt(num) % 9 == 0){
+                res += num;
+            }
+        }
+        System.out.println(res);
     }
 }
