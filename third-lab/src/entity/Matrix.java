2519bf671b95f44bcdb11f6ba3e9a90fa41c9f34
Andrey Shpak <anshpak.by@gmail.com>
2023-03-23 01:55:22 +0300

    feat: refuce the idea with generic class

--- third-lab/src/entity/Matrix.java
+++ third-lab/src/entity/Matrix.java
@@ -1,15 +1,22 @@
 package entity;
 import exception.MatrixException;
 
-public class Matrix<T extends Number> {
-    private T[][] matrix;
-    public Matrix(T[][] matrix) {
+public class Matrix {
+    private int[][] matrix;
+    public Matrix(int[][] matrix) {
         this.matrix = matrix;
     }
     public Matrix(int n, int m) throws MatrixException {
         if(n < 1 || m < 1) {
             throw new MatrixException();
         }
-        matrix = new T[n][m];
+        matrix = new int[n][m];
     }
+    public int getVerticalSize() {
+        return matrix.length;
+    }
+    public int getHorizontalSize() {
+        return matrix[0].length;
+    }
+    
 }
