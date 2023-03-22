ed9e17f83a6eed6c6d62519625e7bda3e9606bd1
Andrey Shpak <anshpak.by@gmail.com>
2023-03-23 02:20:12 +0300

    feat: add package creator and class MatrixCreator with method fillRandomized

---
+++ third-lab/src/creator/MatrixCreator.java
@@ -0,0 +1,20 @@
+package creator;
+import entity.Matrix;
+import exception.MatrixException;
+public class MatrixCreator {
+    public void fillRandomized(Matrix matrix, int minValue, int maxValue) {
+        int v = matrix.getVerticalSize();
+        int h = matrix.getHorizontalSize();
+        for(int i = 0; i < v; i++) {
+            for(int j = 0; j < h; j++) {
+                try {
+                    int value = (int)((Math.random() * (maxValue - minValue)) + minValue);
+                    matrix.setElement(i, j, value);
+
+                } catch (MatrixException e) {
+                    // exception impossible
+                }
+            }
+        }
+    }
+}
