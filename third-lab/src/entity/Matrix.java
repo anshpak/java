7ea38223ef2dadb6c6a4772705be59fc00f1ac63
Andrey Shpak <anshpak.by@gmail.com>
2023-03-23 01:28:02 +0300

    feat: add generic and try to deal with generic non instance situation

--- third-lab/src/entity/Matrix.java
+++ third-lab/src/entity/Matrix.java
@@ -1,11 +1,15 @@
 package entity;
+import exception.MatrixException;
 
-public class Matrix {
-    private byte[][] matrix;
-
-    public Matrix(byte[][] matrix){
+public class Matrix<T extends Number> {
+    private T[][] matrix;
+    public Matrix(T[][] matrix) {
         this.matrix = matrix;
     }
-
-
+    public Matrix(int n, int m) throws MatrixException {
+        if(n < 1 || m < 1) {
+            throw new MatrixException();
+        }
+        matrix = new T[n][m];
+    }
 }
