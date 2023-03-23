55cb33df597748c101aba187024befb399f46969
Andrey Shpak <anshpak.by@gmail.com>
2023-03-23 03:32:44 +0300

    feat: add AND, NOT methods in class Logical and test them

--- third-lab/src/action/Logical.java
+++ third-lab/src/action/Logical.java
@@ -4,15 +4,13 @@ import exception.MatrixException;
 
 public class Logical {
     public final Matrix OR(Matrix boolMatrix1, Matrix boolMatrix2) throws MatrixException {
-        int v1 = boolMatrix1.getVerticalSize();
-        int h1 = boolMatrix1.getHorizontalSize();
-        int v2 = boolMatrix2.getVerticalSize();
-        int h2 = boolMatrix2.getHorizontalSize();
-        if (checkSizes(v1, h1, v2, h2)) {
+        if (checkSizes(boolMatrix1, boolMatrix2)) {
+            int v1 = boolMatrix1.getVerticalSize();
+            int h1 = boolMatrix1.getHorizontalSize();
             Matrix resultMatrix = new Matrix(v1, h1);
             for(int i = 0; i < v1; i++) {
                 for (int j = 0; j < h1; j++) {
-                    if (boolMatrix1.getElement(i, j) + boolMatrix2.getElement(i, j) == 1) {
+                    if (boolMatrix1.getElement(i, j) + boolMatrix2.getElement(i, j) == 1 || boolMatrix1.getElement(i, j) + boolMatrix2.getElement(i, j) == 2) {
                         resultMatrix.setElement(i, j, 1);
                     } else {
                         resultMatrix.setElement(i, j, 0);
@@ -25,7 +23,47 @@ public class Logical {
         }
     }
 
-    private boolean checkSizes(int v1, int h1, int v2, int h2) {
+    public final Matrix AND(Matrix boolMatrix1, Matrix boolMatrix2) throws MatrixException {
+        if (checkSizes(boolMatrix1, boolMatrix2)) {
+            int v1 = boolMatrix1.getVerticalSize();
+            int h1 = boolMatrix1.getHorizontalSize();
+            Matrix resultMatrix = new Matrix(v1, h1);
+            for(int i = 0; i < v1; i++) {
+                for (int j = 0; j < h1; j++) {
+                    if (boolMatrix1.getElement(i, j) * boolMatrix2.getElement(i, j) == 1) {
+                        resultMatrix.setElement(i, j, 1);
+                    } else {
+                        resultMatrix.setElement(i, j, 0);
+                    }
+                }
+            }
+            return resultMatrix;
+        } else {
+            throw new MatrixException();
+        }
+    }
+
+    public final Matrix NOT(Matrix boolMatrix) throws MatrixException {
+        int v = boolMatrix.getVerticalSize();
+        int h = boolMatrix.getHorizontalSize();
+        Matrix resultMatrix = new Matrix(v, h);
+        for(int i = 0; i < v; i++) {
+            for (int j = 0; j < h; j++) {
+                if (boolMatrix.getElement(i, j) == 1) {
+                    resultMatrix.setElement(i, j, 0);
+                } else {
+                    resultMatrix.setElement(i, j, 1);
+                }
+            }
+        }
+        return resultMatrix;
+    }
+
+    private boolean checkSizes(Matrix boolMatrix1, Matrix boolMatrix2) {
+        int v1 = boolMatrix1.getVerticalSize();
+        int h1 = boolMatrix1.getHorizontalSize();
+        int v2 = boolMatrix2.getVerticalSize();
+        int h2 = boolMatrix2.getHorizontalSize();
         return (v1 == v2 && h1 == h2);
     }
 }
--- third-lab/src/main/Main.java
+++ third-lab/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 import entity.Matrix;
 import exception.MatrixException;
 import creator.MatrixCreator;
+import action.Logical;
 
 public class Main {
     public static void main(String[] args) throws MatrixException {
@@ -10,8 +11,24 @@ public class Main {
         final int minValue = 0;
         final int maxValue = 2;
         Matrix boolMatrix1 = new Matrix(N, M);
+        Matrix boolMatrix2 = new Matrix(N, M);
         MatrixCreator creator = new MatrixCreator();
         creator.fillRandomized(boolMatrix1, minValue, maxValue);
+        creator.fillRandomized(boolMatrix2, minValue, maxValue);
+        Logical logical = new Logical();
+        System.out.print("First one:");
         System.out.print(boolMatrix1);
+        System.out.print("Second one:");
+        System.out.print(boolMatrix2);
+        System.out.print("OR:");
+        Matrix resultMatrix;
+        resultMatrix = logical.OR(boolMatrix1, boolMatrix2);
+        System.out.print(resultMatrix);
+        System.out.print("AND:");
+        resultMatrix = logical.AND(boolMatrix1, boolMatrix2);
+        System.out.print(resultMatrix);
+        System.out.print("NOT:");
+        resultMatrix = logical.NOT(resultMatrix);
+        System.out.print(resultMatrix);
     }
 }
