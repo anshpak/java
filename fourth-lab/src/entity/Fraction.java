4a5ad0657ad38994f396bea29002ea92cbd7cafe
Andrey Shpak <anshpak.by@gmail.com>
2023-03-29 18:44:54 +0300

    feat: add methods for fraction reduce; didn't test yet

--- fourth-lab/src/entity/Fraction.java
+++ fourth-lab/src/entity/Fraction.java
@@ -1,6 +1,8 @@
 package entity;
 import exception.FractionException;
 
+import java.util.ArrayList;
+
 public class Fraction extends Number{
     private int numerator;
     private int denomerator;
@@ -8,6 +10,10 @@ public class Fraction extends Number{
         if (checkDenomenator(denomerator)) {
             this.numerator = numerator;
             this.denomerator = denomerator;
+        } else if (numerator == 0) {
+            throw new FractionException("Pointless use of class.");
+        } else {
+            throw new FractionException("Division by zero.");
         }
 
     }
@@ -17,6 +23,36 @@ public class Fraction extends Number{
             return false;
         }
         return true;
+    }
+
+    private Fraction reduceFraction(int numerator, int denomerator) {
+        ArrayList<Integer> numMultipliers = getMultipliers(numerator);
+        ArrayList<Integer> denomMultipliers = getMultipliers(denomerator);
+        for (int multiplier: numMultipliers) {
+            if(denomMultipliers.contains(multiplier)) {
+                numerator /= multiplier;
+                denomerator /= multiplier;
+                denomMultipliers.remove(multiplier);
+            }
+        }
+        return null;
+    }
+
+    private ArrayList<Integer> getMultipliers(int number) {
+        ArrayList<Integer> multipliers = new ArrayList<Integer>();
+        while(number % 2 == 0) {
+            multipliers.add(2);
+            number = number / 2;
+        }
+        for(int i = 3; i * i <= number; i += 2) {
+            if(number % i == 0) {
+                multipliers.add(i);
+                number = number / i;
+                i = 3;
+            }
+        }
+        multipliers.add(number);
+        return multipliers;
     }
 
     public int getNumerator() {
--- fourth-lab/src/main/Main.java
+++ fourth-lab/src/main/Main.java
@@ -1,11 +1,16 @@
 package main;
 import entity.Fraction;
 import exception.FractionException;
+import entity.Fraction;
 
 public class Main {
     public static void main(String[] args) throws FractionException {
         int n = 1;
-        long m = 3;
-
+        int m = 3;
+        // Division by zero example
+        // Fraction errorFrac = new Fraction(1, 0);
+        // Pointless use of the class example
+        //Fraction errorFrac = new Fraction(0, 0);
+        Fraction fraction = new Fraction(15, 1);
     }
 }
