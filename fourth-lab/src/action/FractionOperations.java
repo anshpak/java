e228f5c2b2c1dae48a94956f48e179c9c1234604
Andrey Shpak <anshpak.by@gmail.com>
2023-03-29 21:50:47 +0300

    feat: add method for division with fractions

--- fourth-lab/src/action/FractionOperations.java
+++ fourth-lab/src/action/FractionOperations.java
@@ -14,14 +14,44 @@ public class FractionOperations {
         return result;
     }
 
-    public Fraction multiply(int num, Fraction fraction) throws FractionException {
-        return multiply(fraction, num);
+    public Fraction multiply(int number, Fraction fraction) throws FractionException {
+        return multiply(fraction, number);
     }
 
     public Fraction multiply(Fraction fraction1, Fraction fraction2) throws FractionException {
         Fraction result = new Fraction(1, 1);
         result.setNumerator(fraction1.getNumerator() * fraction2.getNumerator());
         result.setDenominator(fraction1.getDenominator() * fraction2.getDenominator());
+        FractionCreation creator = new FractionCreation();
+        creator.reduceFractionCall(result);
+        return result;
+    }
+
+    public Fraction divide(Fraction fraction, int number) throws FractionException {
+        if(number == 0) {
+            throw new FractionException("Division by zero.");
+        }
+        Fraction result = new Fraction(1, 1);
+        result.setNumerator(fraction.getNumerator());
+        result.setDenominator(fraction.getDenominator() * number);
+        FractionCreation creator = new FractionCreation();
+        creator.reduceFractionCall(result);
+        return result;
+    }
+
+    public Fraction divide(int number, Fraction fraction) throws FractionException {
+        Fraction result = new Fraction(1, 1);
+        result.setNumerator(fraction.getDenominator() * number);
+        result.setDenominator(fraction.getNumerator());
+        FractionCreation creator = new FractionCreation();
+        creator.reduceFractionCall(result);
+        return result;
+    }
+
+    public Fraction divide(Fraction fraction1, Fraction fraction2) throws FractionException {
+        Fraction result = new Fraction(1, 1);
+        result.setNumerator(fraction1.getNumerator() * fraction2.getDenominator());
+        result.setDenominator(fraction1.getDenominator() * fraction2.getNumerator());
         FractionCreation creator = new FractionCreation();
         creator.reduceFractionCall(result);
         return result;
--- fourth-lab/src/main/Main.java
+++ fourth-lab/src/main/Main.java
@@ -14,8 +14,10 @@ public class Main {
         //Fraction errorFrac = new Fraction(0, 0);
         Fraction fraction1 = new Fraction(156384, 1242648);
         Fraction fraction2 = new Fraction(11, 57);
+        Fraction fraction3 = new Fraction(2, 523);
         int num1 = 523;
         int num2 = 11;
+        int num3 = 2;
         FractionOperations operations = new FractionOperations();
 
         System.out.print("Fraction 1:\n");
@@ -23,6 +25,10 @@ public class Main {
         System.out.print("\n\nMultiplication fraction by number:\n" + fraction1 + " * " + num1 + " = " + operations.multiply(fraction1, num1));
         System.out.print("\n\nMultiplication number by fraction:\n" + num2 + " * " + fraction1 + " = " + operations.multiply(num2, fraction1));
         System.out.print("\n\nMultiplication fraction by fraction:\n" + fraction1 + " * " + fraction2 + " = " + operations.multiply(fraction1, fraction2));
-
+        System.out.print("\n\nDivision fraction by number:\n" + fraction1 + " / " + num3 + " = " + operations.divide(fraction1, num3));
+        System.out.print("\n\nDivision number by fraction:\n" + num3 + " / " + fraction1 + " = " + operations.divide(num1, fraction1));
+        System.out.print("\n\nDivision fraction by fraction:\n" + fraction1 + " / " + fraction3 + " = " + operations.divide(fraction1, fraction3));
+        // Division by zero example
+        // operations.divide(fraction1, 0);
     }
 }
