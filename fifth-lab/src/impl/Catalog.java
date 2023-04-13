dca2e5b5444d870bcd1d359d8d63ed7aeca5d9d4
Andrey Shpak <anshpak.by@gmail.com>
2023-04-13 09:02:16 +0300

    feat: fix mistakes and add more test to main

---
+++ fifth-lab/src/impl/Catalog.java
@@ -0,0 +1,36 @@
+package Entity;
+
+import action.CatalogAction;
+import impl.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog implements CatalogAction {
+    private List<Product> products;
+
+    public Catalog() {
+        products = new ArrayList<>();
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void showCatalog() {
+        for (Product pr:this.getProducts()
+        ) {
+            System.out.print(pr);
+        }
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+    @Override
+    public void removeProduct(Product product) {
+        this.products.remove(product);
+    }
+}
