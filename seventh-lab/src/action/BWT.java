e6afbe59ac0f65fb5dd82ee664bdb05e3359b2b3
Andrey Shpak <anshpak.by@gmail.com>
2023-04-19 02:50:58 +0300

    feat: add class and staic method for BW decoding

--- seventh-lab/src/action/BWT.java
+++ seventh-lab/src/action/BWT.java
@@ -1,4 +1,48 @@
 package action;
 
+import java.util.Arrays;
+
 public class BWT {
+    public static String decode(String L) {
+        String res = "";
+        String F = BWT.sortString(L);
+        int index = L.length() - 1;
+        int count = L.length();
+        char ch;
+        while(count != 0) {
+            ch = F.charAt(index);
+            res += ch;
+            index = BWT.getNextIndex(index, ch, L, F);
+            count--;
+        }
+
+        return res;
+    }
+
+    private static int getNextIndex(int curInd, char ch, String L, String F) {
+        int count = 0;
+        for(int i = 0; i < curInd; i++) {
+            if(F.charAt(i) == ch) {
+                count++;
+            }
+        }
+        int j = 0;
+        while(true) {
+            if(L.charAt(j) == ch) {
+                count--;
+                if (count < 0) {
+                    break;
+                }
+            }
+            j++;
+        }
+        return j;
+    }
+
+    private static String sortString(String inputString)
+    {
+        char tempArray[] = inputString.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
 }
