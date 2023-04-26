4901f9c357e9c1c19814ed4a4eea8b41250ef3c1
Andrey Shpak <anshpak.by@gmail.com>
2023-04-26 16:25:21 +0300

    feat: add new classes Segment, SegmentAction, TreeCreator

---
+++ ninth-lab/src/action/SegmentAction.java
@@ -0,0 +1,16 @@
+package action;
+
+import entity.Point;
+import entity.Segment;
+
+import java.util.ArrayList;
+
+public class SegmentAction {
+    public static ArrayList<Point> getIntersectionPoints(ArrayList<Segment> segments) {
+        for (int i = 0; i < segments.size(); i++) {
+            for (int j = 0; j < segments.size(); j++) {
+
+            }
+        }
+    }
+}
---
+++ ninth-lab/src/creator/TreeCreator.java
@@ -0,0 +1,15 @@
+package creator;
+
+import action.SegmentAction;
+import entity.Point;
+import entity.Segment;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class TreeCreator {
+    public static void fillTreeWithAbscissa(TreeMap<Integer, Point> tree, ArrayList<Segment> segments) {
+        ArrayList<Point> intersPoints = new ArrayList<>();
+        intersPoints = SegmentAction.getIntersectionPoints(segments);
+    }
+}
